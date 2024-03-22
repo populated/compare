388d216ab5aa66afcf8358e085a58dde21289352
alluding <149972831+alluding@users.noreply.github.com>
2024-03-22 05:47:52 -0400

    Create typing.py

---
+++ compare/typing.py
@@ -0,0 +1,25 @@
+from typing import (
+    Any,
+    Literal,
+    Union,
+    Optional,
+    Dict,
+    Type,
+    Tuple,
+    Annotated,
+    TYPE_CHECKING,
+    List
+)
+
+__all__ = [
+    "Any",
+    "Literal",
+    "Union",
+    "Optional",
+    "Dict",
+    "Type",
+    "Tuple",
+    "Annotated",
+    "TYPE_CHECKING",
+    "List"
+]
