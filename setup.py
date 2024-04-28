42ad0f12763dd9d7384510a231666a7811c06347
alluding <149972831+alluding@users.noreply.github.com>
2024-04-27 20:27:05 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import re
+
+version = "1.0.1"
+if not version:
+    raise RuntimeError("Version is not set!")
+
+setup(
+    name="pysimilarities",
+    author="alluding",
+    version=version,
+    url="https://github.com/alluding/compare",
+    license="MIT",
+    description="A Python wrapper for comparing texts for similarities.",
+    install_requires=[
+        "typing",
+        "typing_extensions",
+        "pydantic",
+        "scikit-learn",
+        "spacy",
+        "numpy"
+    ],
+    python_requires=">=3.8.0",
+)
