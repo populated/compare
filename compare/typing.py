4ab29e8ba46da873d31416bb04a27792f237aef0
dancers <149972831+alluding@users.noreply.github.com>
2024-05-04 06:27:27 -0400

    Update typing.py

--- compare/typing.py
+++ compare/typing.py
@@ -1,25 +1,13 @@
 from typing import (
-    Any,
-    Literal,
     Union,
-    Optional,
     Dict,
-    Type,
     Tuple,
-    Annotated,
-    TYPE_CHECKING,
     List
 )
 
 __all__ = [
-    "Any",
-    "Literal",
     "Union",
-    "Optional",
     "Dict",
-    "Type",
     "Tuple",
-    "Annotated",
-    "TYPE_CHECKING",
     "List"
 ]
