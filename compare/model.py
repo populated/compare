99307bed40ed8ddd1fab29f02cf348ff5d5c259b
dancers <149972831+alluding@users.noreply.github.com>
2024-05-08 13:12:40 -0400

    Update model.py

--- compare/model.py
+++ compare/model.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from .typing import Union, List, Dict, Tuple
+from pydantic import BaseModel
 import numpy as np
 
 Response = Dict[str, Union[str, float]]
@@ -10,3 +11,8 @@ SIMILARITY_THRESHOLD = 0.6
 TextEmbedding = np.ndarray
 TextPair = Tuple[str, str]
 SimilarityScore = float
+
+class ResultScore(BaseModel):
+  closest: str
+  score: Union[int, float]
+  similar: List[str]
